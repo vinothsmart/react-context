3890843ceea94f8a77fb147600a3ce224150faee
vinothsmart <s.vinothkanna90@gmail.com>
2021-06-09 11:40:39 +0530

    Nav added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import MovieList from "./MovieList";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="App">
       {/* <h1>Let's Code</h1> */}
+      <Nav />
       <MovieList />
     </div>
   );
---
+++ src/Nav.js
@@ -0,0 +1,10 @@
+const Nav = () => {
+  return (
+    <nav>
+      <h2>Vinoth</h2>
+      <h2>No of Movies:</h2>
+    </nav>
+  );
+};
+
+export default Nav;
